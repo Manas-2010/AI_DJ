5259729e6d23748426feffb4e3a76ad087076383
Manas-2010 <91479346+Manas-2010@users.noreply.github.com>
2022-06-29 19:51:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload(){
     song = loadSound('music.mp3');
@@ -10,6 +14,24 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X: " + leftWristX + " Left Wrist Y: " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X: " + rightWristX + " Right Wrist Y: " + rightWristY);
+    }
 }
 
 function draw() {
@@ -18,4 +40,6 @@ function draw() {
 
 function play() {
     song.play();
+    song.rate(1);
+    song.volume(1);
 }
